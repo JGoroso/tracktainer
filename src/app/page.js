e67609384436b4a7754e3c78550b986180fd129c
Joel Goroso <yoelgoroso@gmail.com>
2023-09-21 20:21:34 -0300

    TAREA-15: Home structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
+"use client"
+import GoogleMapView from "./components/GoogleMapView";
+import NavBar from "./components/Navbar";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    </main>
+    <div className="grid grid-cols-4 h-screen">
+      <div className='bg-white-300'>
+        <NavBar />
+      </div>
+      <div className='col-span-3'>
+        <GoogleMapView />
+      </div>
+    </div>
   )
 }
