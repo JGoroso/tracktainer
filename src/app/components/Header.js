de10e0b8300a7b8c53bc86baa3a30fbb3a55fdfb
Joel Goroso <yoelgoroso@gmail.com>
2023-09-22 18:56:34 -0300

    TAREA-15: Header fixes

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,33 +2,36 @@
 import Image from 'next/image'
 import React from 'react'
 import { useSession } from "next-auth/react"
+import { useRouter } from 'next/navigation'
 
 function Header() {
+  const router = useRouter();
+  const goHome = () => {
+    router.push('/');
+  };
   const { data: session } = useSession()
   return (
-    <div className="grid grid-cols-2  w-full text-center text-xs text-forestgreen font-roboto">
-      <div className="flex items-center text-left text-base text-black" >
-        <Image
-          alt=""
-          width={40}
-          height={40}
-          src="/logo.png"
-        />
-        <div className="font-medium">
-          TrackTainer
+    <div className="grid grid-cols-2 h-12 w-full">
+      <button onClick={goHome}>
+        <div className="flex items-center" >
+          <Image
+            alt=""
+            width={40}
+            height={40}
+            src="/logo.png"
+          />
+          <div className="text-base left text-black">
+            TrackTainer
+          </div>
         </div>
-      </div>
+      </button>
       <div className="flex justify-end mr-1">
         {session?.user ? <Image src={session.user.image}
           alt="user"
           width={50}
           height={50}
-          className="rounded-full" /> : <Image src="/user.png"
-            width={40}
-            height={40}
-            alt="userdefault"
-
-        />}
+          className="rounded-full" /> : null
+        }
       </div>
     </div>
   )
