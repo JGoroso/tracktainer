3e18608f9dc111ab4baa7de9d5cacd98eaefe7f7
Joel Goroso <yoelgoroso@gmail.com>
2023-09-26 21:45:08 -0300

    TT-19: GoogleMapView GET Firestore documents and markers

--- src/app/components/GoogleMapView.js
+++ src/app/components/GoogleMapView.js
@@ -1,8 +1,34 @@
 import { GoogleMap, Marker } from '@react-google-maps/api'
 import { LoadScript } from '@react-google-maps/api'
-import React from 'react'
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import app from '../firebase/firebase';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+// se repite en dos oportunidades, refactor.
+const db = getFirestore(app)
 
 function GoogleMapView() {
+
+  const [markers, setMarkers] = useState([])
+  let markersArray = []
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'pedidos'));
+        const newData = [];
+        querySnapshot.forEach((doc) => {
+          newData.push(doc.data());
+        });
+        setMarkers(newData);
+      } catch (error) {
+        console.error('Error al obtener datos de Firestore:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const containerStyle = {
     width: '100%',
@@ -16,6 +42,7 @@ function GoogleMapView() {
 
       <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY} mapIds={['ffac895524fa317e']}>
         <GoogleMap mapContainerStyle={containerStyle} center={cordinate} zoom={15} options={{ mapId: 'ffac895524fa317e' }}>
+          {markers.map((marker) => <Marker key={marker.cliente} position={{ lat: marker.lat, lng: marker.lng }} icon={{ url: "/container.png" }} />)}
         </GoogleMap>
       </LoadScript>
     </div>
