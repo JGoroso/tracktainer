c69bf04e09d4c6355be8adbff113c1d0c533a88d
Joel Goroso <yoelgoroso@gmail.com>
2023-10-02 23:09:00 -0300

    TT-33 Initial commit

--- src/app/Agregarpedidos/page.js
+++ src/app/Agregarpedidos/page.js
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react'
-import NuevoPedidoForm from '../components/nuevoPedidoForm'
+import NuevoPedidoForm from '../components/NuevoPedidoForm'
 
 export default function page() {
   return <NuevoPedidoForm />
