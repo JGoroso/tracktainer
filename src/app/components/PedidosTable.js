8056b387719f881bbcb05e879380882d336fcaa7
Joel Goroso <yoelgoroso@gmail.com>
2023-10-14 16:30:36 -0300

    TT-43: Finalizado proceso update desde /Pedidos

--- src/app/components/PedidosTable.js
+++ src/app/components/PedidosTable.js
@@ -1,22 +1,36 @@
 'use client'
 import React from 'react'
-import { getContainers } from '../firebase/firestore/firestore'
+import { deleteContainer, getContainers } from '../firebase/firestore/firestore'
 import { useAsync } from '../hooks/useAsync'
 import { useState } from 'react'
 import UpdateForm from './UpdateForm'
 import { Menu, Transition } from '@headlessui/react'
 import { Fragment } from 'react'
 function PedidosTable() {
-  const [showModal, setShowModal] = useState(false);
+  const [elementId, setElementId] = useState("")
+  const [estado, setActualEstado] = useState("")
+  const [showModal, setShowModal] = useState(false)
+  const [refresh, setRefresh] = useState(false)
+
+
+  const refreshConteiners = () => {
+    setRefresh(true)
+    setTimeout(() => {
+      setRefresh(false)
+    }, 3000);
+
+  }
+
   // Se llama a la funcion getContainers que nos devuelve todos los objetos de la coleccion 'Pedidos' en forma de promesa
   const getContainersFromFirestore = () => getContainers()
   // Utilizamos un hook creado por nosotros que nos permite pasarle esta promesa que contiene un array de todos los objetos y con esta
   // podremos recibir la data utilizando un useEffect y luego utilizar estos datos donde queramos
-  const { data } = useAsync(getContainersFromFirestore, '')
+  const { data } = useAsync(getContainersFromFirestore, refresh)
 
   const closeModal = () => {
     setShowModal(false);
   };
+
 
 
   return (
@@ -26,22 +40,16 @@ function PedidosTable() {
           <div className="flex items-center gap-x-3">
             <h2 className="font-medium text-xl text-gray-800 ">Pedidos</h2>
           </div>
-
           <p className="mt-1 text-sm text-gray-800 ">En esta sección se encontraran los pedidos pendientes y entregados</p>
         </div>
 
         <div className="flex items-center mt-4 gap-x-3">
           <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto ray-800 0 hover:bg-gray-100 200 y-700">
-            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <g clipPath="url(#clip0_3098_154395)">
-                <path d="M13.3333 13.3332L9.99997 9.9999M9.99997 9.9999L6.66663 13.3332M9.99997 9.9999V17.4999M16.9916 15.3249C17.8044 14.8818 18.4465 14.1806 18.8165 13.3321C19.1866 12.4835 19.2635 11.5359 19.0351 10.6388C18.8068 9.7417 18.2862 8.94616 17.5555 8.37778C16.8248 7.80939 15.9257 7.50052 15 7.4999H13.95C13.6977 6.52427 13.2276 5.61852 12.5749 4.85073C11.9222 4.08295 11.104 3.47311 10.1817 3.06708C9.25943 2.66104 8.25709 2.46937 7.25006 2.50647C6.24304 2.54358 5.25752 2.80849 4.36761 3.28129C3.47771 3.7541 2.70656 4.42249 2.11215 5.23622C1.51774 6.04996 1.11554 6.98785 0.935783 7.9794C0.756025 8.97095 0.803388 9.99035 1.07431 10.961C1.34523 11.9316 1.83267 12.8281 2.49997 13.5832" stroke="currentColor" strokeWidth="1.67" strokeLinecap="round" strokeLinejoin="round" />
-              </g>
-              <defs>
-                <clipPath id="clip0_3098_154395">
-                  <rect width="20" height="20" fill="white" />
-                </clipPath>
-              </defs>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
             </svg>
+
 
             <span>Volver al mapa</span>
           </button>
@@ -92,56 +100,56 @@ function PedidosTable() {
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200 y-700 0">
-                  {data && data.map((marker) =>
-                    <tr>
+                  {data && data.map((element) =>
+                    <tr key={element.id}>
                       <td className="px-2 py-3.5 text-sm font-medium whitespace-nowrap">
                         <div>
-                          <h2 className="font-medium text-gray-800  ">{marker.id}</h2>
+                          <h2 className="font-medium text-gray-800  ">{element.id}</h2>
                         </div>
                       </td>
                       <td className="px-2 py-4 text-sm font-medium whitespace-nowrap">
-                        <div className={`inline px-3 py-1 capitalize text-sm font-normal rounded-full ${marker.estado == 'entregado' ? 'text-emerald-500 gap-x-2 bg-emerald-100/60 0' : ' text-orange-500 gap-x-2 bg-orange-100/60 0'}   `}>
-                          {marker.estado}
+                        <div className={`inline px-3 py-1 capitalize text-sm font-normal rounded-full ${element.estado == 'entregado' ? 'text-emerald-500 gap-x-2 bg-emerald-100/60 0' : ' text-orange-500 gap-x-2 bg-orange-100/60 0'}   `}>
+                          {element.estado}
                         </div>
                       </td>
                       <td className="px-2 py-4 text-sm whitespace-nowrap">
                         <div>
-                          <h4 className="text-gray-700 200">{marker.fechaPedido}</h4>
+                          <h4 className="text-gray-700 200">{element.fechaPedido}</h4>
                         </div>
                       </td>
                       <td className="px-4 py-4 text-sm whitespace-nowrap">
                         <div className="flex items-center">
-                          <h4 className="text-gray-700 200">{marker.direccion.split(',')[0]}</h4>
+                          <h4 className="text-gray-700 200">{element.direccion.split(',')[0]}</h4>
                         </div>
                         <h4 className="text-gray-700 200">Córdoba</h4>
                       </td>
 
                       <td className="px-2 py-4 text-sm whitespace-nowrap">
                         <div>
-                          <h4 className="text-gray-700 200">{marker.cliente}</h4>
+                          <h4 className="text-gray-700 200">{element.cliente}</h4>
                         </div>
                       </td>
                       <td className="px-2 py-4 text-sm whitespace-nowrap">
                         <div>
-                          <h4 className="text-gray-700 200">{marker.nombreCliente}</h4>
-                        </div>
-                      </td>
-
-                      <td className="px-2 py-4 text-sm whitespace-nowrap">
-                        <div>
-                          <h4 className="text-gray-700 200">{marker.telefono}</h4>
+                          <h4 className="text-gray-700 200">{element.nombreCliente}</h4>
                         </div>
                       </td>
 
                       <td className="px-2 py-4 text-sm whitespace-nowrap">
                         <div>
-                          <h4 className="text-gray-700 200">{marker.idContenedor}</h4>
+                          <h4 className="text-gray-700 200">{element.telefono}</h4>
                         </div>
                       </td>
 
                       <td className="px-2 py-4 text-sm whitespace-nowrap">
                         <div>
-                          <h4 className="text-gray-700 200">{marker.chofer}</h4>
+                          <h4 className="text-gray-700 200">{element.idContenedor}</h4>
+                        </div>
+                      </td>
+
+                      <td className="px-2 py-4 text-sm whitespace-nowrap">
+                        <div>
+                          <h4 className="text-gray-700 200">{element.chofer}</h4>
                         </div>
                       </td>
                       <td className="px-2 py-4 text-sm whitespace-nowrap">
@@ -168,7 +176,7 @@ function PedidosTable() {
                                   <a
                                     href="#"
                                     className={(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                    onClick={() => { setShowModal(true) }}
+                                    onClick={() => { setShowModal(true); setElementId(element.id); setActualEstado(element.estado) }}
                                   >
                                     Editar
                                   </a>
@@ -179,6 +187,7 @@ function PedidosTable() {
                                   <a
                                     href="#"
                                     className={(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                    onClick={active ? deleteContainer(element.id) : null}
                                   >
                                     Borrar Contenedor
                                   </a>
@@ -196,7 +205,7 @@ function PedidosTable() {
           </div>
         </div>
       </div>
-      {showModal ? <UpdateForm closeModal={closeModal} /> : null}
+      {showModal ? <UpdateForm closeModal={closeModal} elementId={elementId} actualEstado={estado} refresh={refreshConteiners} /> : null}
     </div >
 
   )
