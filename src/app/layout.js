85e372cddf6aa19baa4da6791120b98b47693486
Joel Goroso <87891576+JGoroso@users.noreply.github.com>
2024-06-11 21:13:32 -0300

    initial commit (#11)

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import { authOptions } from '../../pages/api/auth/[...nextauth]'
 import Header from './components/Header'
 import { LoginPage } from './Login/login'
 import UserNotFound from './components/UserNotFound'
+import NextTopLoader from 'nextjs-toploader';
 
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['500'] })
@@ -17,10 +18,10 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   const session = await getServerSession(authOptions)
 
-  console.log(session)
   return (
     <html lang="en">
       <body className="h-screen bg-gradient-to-b from-slate-100 to-slate-100">
+        <NextTopLoader />
         <main className={roboto.className}>
           <Provider>
             {(session && session?.user?.userAllowed != false) ? (
@@ -29,11 +30,12 @@ export default async function RootLayout({ children }) {
                 {children}
               </>
             ) : (session?.user?.userAllowed == false) ? <UserNotFound /> :
-              < LoginPage />
+              <LoginPage />
             }
           </Provider>
+
         </main>
       </body>
-    </html>
+    </html >
   )
 }
