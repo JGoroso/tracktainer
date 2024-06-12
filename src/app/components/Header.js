043e612685ae7f60cb2d85fa56f50a3615b1289a
Joel Goroso <yoelgoroso@gmail.com>
2024-06-12 12:06:31 -0300

    navbar cosmetics

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -8,37 +8,6 @@ import NavBar from './Navbar'
 export default function Header() {
 
   return (
-    <div className="grid grid-cols-2 h-30 gap-4">
-      <div className="flex items-center justify-start">
-        <Link href={"/"}>
-          <button>
-            <div className="flex items-center justify-center">
-              <Image
-                alt=""
-                width={50}
-                height={30}
-                src="/logo.png"
-                priority
-              />
-              <div className="text-black text-xl">
-                TrackTainer
-              </div>
-              <Image
-                className='ml-4'
-                alt=""
-                width={60}
-                height={40}
-                src="/trejologo3.png"
-                priority
-              />
-            </div>
-          </button>
-        </Link>
-      </div>
-      <div className="flex items-center justify-end">
-        <NavBar />
-      </div>
-
-    </div>
+    <NavBar />
   )
 }
