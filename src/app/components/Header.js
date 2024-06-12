c3f5a25cf7485566d4ea4443193dd416a609b9c7
Joel Goroso <yoelgoroso@gmail.com>
2024-06-11 21:12:49 -0300

    initial commit

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,15 +1,15 @@
 "use client"
 import Image from 'next/image'
 import React from 'react'
-import { signOut, useSession } from "next-auth/react"
+
 import Link from 'next/link'
 import NavBar from './Navbar'
 
 export default function Header() {
-  const { data: session } = useSession()
+
   return (
     <div className="grid grid-cols-2 h-30 gap-4">
-      <div>
+      <div className="flex items-center justify-start">
         <Link href={"/"}>
           <button>
             <div className="flex items-center justify-center">
@@ -35,28 +35,10 @@ export default function Header() {
           </button>
         </Link>
       </div>
-      <div className="flex self-center w-full justify-end">
+      <div className="flex items-center justify-end">
         <NavBar />
-        <div className='flex ml-4'>
-          <div>
-            {session?.data?.user.name}
-            {session?.user ? <>{session.user.name}<Image src={session.user.image}
-              alt="user"
-              width={50}
-              height={50}
-              className="rounded-full" /></> : null
-            }
-          </div>
-          <div>
-            <button onClick={() => signOut()} className="self-center mb-4">
-              <svg className="fill-stroke self-center" width="46" height="45" viewBox="0 0 54 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M40.375 32L49.875 24M49.875 24L40.375 16M49.875 24L16.625 24M30.875 32V34C30.875 37.3137 27.685 40 23.75 40L14.25 40C10.315 40 7.125 37.3137 7.125 34L7.125 14C7.125 10.6863 10.315 8 14.25 8L23.75 8C27.685 8 30.875 10.6863 30.875 14V16" stroke="#545F71" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-              </svg>
-              <p className="self-center text-base text-[#545F71]">Salir</p>
-            </button>
-          </div>
-        </div>
       </div>
-    </div >
+
+    </div>
   )
 }
