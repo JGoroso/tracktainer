f0f5c82f671f7c9f6ede448d6af778ce68c9df1d
Joel Goroso <yoelgoroso@gmail.com>
2024-09-01 06:34:34 -0300

    check table

--- src/app/components/dashboard-componentes/Dashboard/Dashboards.tsx
+++ src/app/components/dashboard-componentes/Dashboard/Dashboards.tsx
@@ -1,16 +1,17 @@
 "use client";
 import dynamic from "next/dynamic";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ChartOne from "../Charts/ChartOne";
 import ChartTwo from "../Charts/ChartTwo";
 import TableOne from "../Tables/TableOne";
+import ExportExcel from "../Export/ExportExcel";
 import CardDataStats from "../CardDataStats";
 import Image from "next/image";
-import axios from "axios"
+import axios from "axios";
 
-  const ChartThree = dynamic(() => import("../Charts/ChartThree"), {
-    ssr: false,
-  });
+const ChartThree = dynamic(() => import("../Charts/ChartThree"), {
+  ssr: false,
+});
 
 const Dashboards: React.FC = () => {
   const [choferes, setChoferes] = useState([]);
@@ -23,19 +24,19 @@ const Dashboards: React.FC = () => {
     const fetchData = async () => {
       try {
         // Realiza una solicitud GET para obtener los choferes
-        const choferesResponse = await axios.get('/api/get/choferes');
+        const choferesResponse = await axios.get("/api/get/choferes");
         setChoferes(choferesResponse.data);
-        
+
         // Realiza una solicitud GET para obtener los contenedores
-        const contenedoresResponse = await axios.get('/api/get/contenedores');
+        const contenedoresResponse = await axios.get("/api/get/contenedores");
         setContenedores(contenedoresResponse.data);
 
         // Realiza una solicitud GET para obtener los pedidos pendientes
-        const pedidosPendientes = await axios.get('/api/get/pedidospendientes');
+        const pedidosPendientes = await axios.get("/api/get/pedidospendientes");
         setPedidosPendientes(pedidosPendientes.data);
 
         // Realiza una solicitud GET para obtener los pedidos entregados
-        const pedidosEntregados = await axios.get('/api/get/pedidosentregados');
+        const pedidosEntregados = await axios.get("/api/get/pedidosentregados");
         setPedidosEntregados(pedidosEntregados.data);
       } catch (error) {
         setError(error.message);
@@ -50,12 +51,23 @@ const Dashboards: React.FC = () => {
   }
   return (
     <>
+      <div className="flex rounded-sm border border-stroke py-2 mb-4">
+        <ExportExcel />
+      </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-          {/* rate="0.43%" levelUp se pueden agregar */}
-        <CardDataStats title="Cantidad contenedores" total={contenedores.length.toString()} rate="">  
-          <Image src="/dumpster.png" width="22" height="22" alt="dumpster"/>
+        {/* rate="0.43%" levelUp se pueden agregar */}
+        <CardDataStats
+          title="Cantidad contenedores"
+          total={contenedores.length.toString()}
+          rate=""
+        >
+          <Image src="/dumpster.png" width="22" height="22" alt="dumpster" />
         </CardDataStats>
-        <CardDataStats title="Cantidad de pedidos entregados (en sitio)" total={pedidosEntregados.length.toString()} rate="">
+        <CardDataStats
+          title="Cantidad de pedidos entregados (en sitio)"
+          total={pedidosEntregados.length.toString()}
+          rate=""
+        >
           <svg
             className="fill-green-400"
             width="22"
@@ -64,10 +76,20 @@ const Dashboards: React.FC = () => {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"/>
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
+            />
           </svg>
         </CardDataStats>
-        <CardDataStats title="Cantidad de pedidos pendientes de entrega" total={pedidosPendientes.length.toString()} rate="" >
+        <CardDataStats
+          title="Cantidad de pedidos pendientes de entrega"
+          total={pedidosPendientes.length.toString()}
+          rate=""
+        >
           <svg
             className="fill-orange-400"
             width="26"
@@ -76,19 +98,34 @@ const Dashboards: React.FC = () => {
             fill=""
             xmlns="http://www.w3.org/2000/svg"
           >
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"/>
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
+            />
           </svg>
         </CardDataStats>
-        <CardDataStats title="Choferes actuales" total={choferes.length.toString()} rate="" >
+        <CardDataStats
+          title="Choferes actuales"
+          total={choferes.length.toString()}
+          rate=""
+        >
           <svg
-            className="fill-gray-800" 
+            className="fill-gray-800"
             width="22"
             height="18"
             viewBox="0 0 22 18"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-         <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M16 19h4a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-2m-2.236-4a3 3 0 1 0 0-4M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth="2"
+              d="M16 19h4a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-2m-2.236-4a3 3 0 1 0 0-4M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
           </svg>
         </CardDataStats>
       </div>
--- src/app/components/dashboard-componentes/Tables/TableOne.tsx
+++ src/app/components/dashboard-componentes/Tables/TableOne.tsx
@@ -1,74 +1,171 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import Image from "next/image";
-
-
+type Pedido = {
+  id: string;
+  estado: string;
+  cliente: string;
+  chofer: string;
+  remito: string;
+  fechaPedido: string;
+};
 
 const TableOne = () => {
+  const [pedidosEntregados, setPedidosEntregados] = useState<Pedido[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Realiza una solicitud GET para obtener los pedidos entregados
+        const response = await axios.get("/api/get/pedidosentregados");
+        setPedidosEntregados(response.data);
+      } catch (error) {
+        setError((error as Error).message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Función para formatear la fecha
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1; // Los meses en JavaScript son basados en 0
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
+  // Parse dates
+  const parseDate = (dateString: string) => new Date(dateString);
+
+  const calculateDaysDifference = (date1: Date, date2: Date) => {
+    const differenceInTime = date1.getTime() - date2.getTime();
+    return Math.ceil(differenceInTime / (1000 * 3600 * 24));
+  };
+
+  // Function to format the difference as a readable string
+  const formatDifferenceString = (daysDifference: number) => {
+    if (daysDifference < 0) return "Aún no entregado";
+    if (daysDifference === 0) return "0";
+    if (daysDifference === 1) return "1 DÍA";
+    if (daysDifference < 30) return `${daysDifference} DÍAS`;
+    return "Más de 30 días";
+  };
+
+  const today = new Date();
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        Pedidos completados 16/06/2024
+        Pedidos con estado <span className="text-green-500">Entregado </span>/
+        Tiempo en sitio
       </h4>
 
-      <div className="flex flex-col">
-        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
-          <div className="p-2.5 xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Estado
-            </h5>
-          </div>
-          <div className="p-2.5 text-center xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Cliente
-            </h5>
-          </div>
-          <div className="p-2.5 text-center xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Chofer
-            </h5>
-          </div>
-          <div className="hidden p-2.5 text-center sm:block xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Remito
-            </h5>
-          </div>
-          <div className="hidden p-2.5 text-center sm:block xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Recibio
-            </h5>
-          </div>
-        </div>
+      <div className="mt-7 overflow-x-auto">
+        <table className="min-w-full table-auto">
+          <thead className="bg-gray-100 font-bold uppercase bg-gray-2 rounded-sm">
+            <tr>
+              <th
+                scope="col"
+                className="px-2 py-3.5 text-sm font-medium text-left text-gray-500"
+              >
+                Estado
+              </th>
+              <th
+                scope="col"
+                className="px-2 py-3.5 text-sm font-medium text-left text-gray-500"
+              >
+                Cliente
+              </th>
+              <th
+                scope="col"
+                className="px-2 py-3.5 text-sm font-medium text-left text-gray-500"
+              >
+                Chofer
+              </th>
+              <th
+                scope="col"
+                className="hidden sm:table-cell px-2 py-3.5 text-sm font-medium text-left text-gray-500"
+              >
+                Remito
+              </th>
+              <th
+                scope="col"
+                className="hidden sm:table-cell px-2 py-3.5 text-sm font-medium text-left text-gray-500"
+              >
+                Fecha entrega
+              </th>
+              <th
+                scope="col"
+                className="hidden sm:table-cell px-2 py-3.5 text-sm font-medium text-left text-gray-500"
+              >
+                Tiempo en sitio (Días)
+              </th>
+            </tr>
+          </thead>
 
-          <div
-            className={`grid grid-cols-3 sm:grid-cols-5`}
-            key={""}
-          >
-            <div className="flex items-center gap-3 p-2.5 xl:p-5">
-              <p className="hidden text-meta-3 sm:block uppercase">
-                Entregado
-              </p>
-            </div>
+          <tbody className="bg-white">
+            {pedidosEntregados &&
+              pedidosEntregados
+                .slice() // Crear una copia del array para evitar mutar el original
+                .sort((a, b) => {
+                  // Convert fechaPedido to Date objects
+                  const dateA = new Date(a.fechaPedido).getTime();
+                  const dateB = new Date(b.fechaPedido).getTime();
 
-            <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-black dark:text-white">Betania</p>
-            </div>
+                  // Ensure the dates are valid numbers
+                  if (isNaN(dateA) || isNaN(dateB)) {
+                    throw new Error("Invalid date format");
+                  }
 
-            <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-black">Ricardo</p>
-            </div>
+                  // Compare the dates
+                  return dateA - dateB;
+                })
+                .map((pedido) => {
+                  const pedidoDate = parseDate(pedido.fechaPedido); // Parse to Date object
+                  const daysDifference = calculateDaysDifference(
+                    today,
+                    pedidoDate
+                  ); // Calculate days difference
+                  const formattedDifference =
+                    formatDifferenceString(daysDifference); // Format difference as string
 
-            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-black dark:text-white">142199</p>
-            </div>
-
-            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-black">JOSE</p>
-            </div>
-          </div>
-      
+                  return (
+                    <tr
+                      key={pedido.id}
+                      className="focus:outline-none border-b border-b-neutral-200"
+                    >
+                      <td className="px-2 py-4 whitespace-nowrap text-base font-medium text-left uppercase text-green-700">
+                        {pedido.estado}
+                      </td>
+                      <td className="px-2 py-4 whitespace-nowrap text-base font-medium text-left text-gray-700">
+                        {pedido.cliente}
+                      </td>
+                      <td className="px-2 py-4 whitespace-nowrap text-base font-medium text-left text-gray-700">
+                        {pedido.chofer}
+                      </td>
+                      <td className="hidden sm:table-cell px-2 py-4 whitespace-nowrap text-base font-medium text-left text-gray-700">
+                        {pedido.remito}
+                      </td>
+                      <td className="hidden sm:table-cell px-2 py-4 whitespace-nowrap text-base font-medium text-left text-gray-700">
+                        {formatDate(pedido.fechaPedido)}
+                      </td>
+                      <td className="hidden sm:table-cell px-2 py-4 whitespace-nowrap text-base font-medium text-left text-gray-700">
+                        {formattedDifference}
+                      </td>
+                    </tr>
+                  );
+                })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 };
-
 export default TableOne;
