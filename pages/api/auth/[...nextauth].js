27ee4c8d4b67bf1ca89661983ca94e331ba440ff
Camila Duarte <camila.mduarte7@gmail.com>
2024-09-09 19:19:10 -0300

    Confirmacion y Pedido Guardado Modal

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,42 +1,45 @@
-
-import NextAuth from "next-auth"
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import { getUsuarios } from "../../../src/app/firebase/firestore/firestore";
-
 
 export const authOptions = {
   secret: process.env.AUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
   callbacks: {
     async session({ session, token }) {
       if (!session) {
         return null;
       } else {
-        // const usersFromFirestore = await getUsuarios();
-        // const allowedEmails = new Set(usersFromFirestore.map(user => user.email));
-        // const isUserAllowed = allowedEmails.has(session?.user?.email);
-        // if (!isUserAllowed) {
-        //   session.user = {
-        //     userAllowed: false
-        //   }
-        //   return Promise.resolve(session)
-        // } else {
-        //   console.log("usuario permitido, iniciando sesión");
-        //   const userRole = usersFromFirestore.find(user => user.email === session?.user.email)?.role;
-        //   // Establecer el rol del usuario en la sesión
-        session.user.email = token.email
-        session.user.userAllowed = true
-        session.user.role = "admin"
+        const usersFromFirestore = await getUsuarios();
+        const allowedEmails = new Set(
+          usersFromFirestore.map((user) => user.email)
+        );
+        const isUserAllowed = allowedEmails.has(session?.user?.email);
+        if (!isUserAllowed) {
+          session.user = {
+            userAllowed: false,
+          };
+          return Promise.resolve(session);
+        } else {
+          console.log("usuario permitido, iniciando sesión");
+          const userRole = usersFromFirestore.find(
+            (user) => user.email === session?.user.email
+          )?.role;
+          // Establecer el rol del usuario en la sesión
+          session.user.email = token.email;
+          session.user.userAllowed = true;
+          session.user.role = userRole;
 
-        return Promise.resolve(session);
+          return Promise.resolve(session);
+        }
       }
-    }
-  }
-}
+    },
+  },
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
---
+++ src/app/components/ConfirmacionModal.js
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import PedidoGuardadoModal from "./PedidoGuardadoModal";
+
+function ConfirmacionModal({ show, onClose, onSave, source }) {
+  const [showPedidoGuardadoModal, setPedidoGuardadoModal] = useState(false);
+
+  if (!show) return null;
+
+  const handleSave = () => {
+    onSave(source);
+    setPedidoGuardadoModal(true);
+    setTimeout(async () => {
+      setPedidoGuardadoModal(false);
+    }, 1500);
+  };
+
+  return (
+    <>
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+        <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <div className="flex items-center justify-between mb-4">
+            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+              Confirmar Datos:
+            </h5>
+          </div>
+          <div className="flow-root">
+            <ul
+              role="list"
+              className="divide-y divide-gray-200 dark:divide-gray-700"
+            >
+              <li className="py-3 sm:py-4">
+                <div className="flex items-center">
+                  <div className="flex-1 min-w-0 ms-4">
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                      Dirección
+                    </p>
+                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                      {source ? source.address : null}
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li className="py-3 sm:py-4">
+                <div className="flex items-center ">
+                  <div className="flex-1 min-w-0 ms-4">
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                      ¿Quién recibe?
+                    </p>
+                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                      {source.recibe}
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li className="py-3 sm:py-4">
+                <div className="flex items-center">
+                  <div className="flex-1 min-w-0 ms-4">
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                      Cliente
+                    </p>
+                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                      {source.cliente}
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li className="py-3 sm:py-4">
+                <div className="flex items-center ">
+                  <div className="flex-1 min-w-0 ms-4">
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                      Teléfono:
+                    </p>
+                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                      {source.telefono_cliente}
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li className="pt-3 pb-0 sm:pt-4">
+                <div className="flex items-center ">
+                  <div className="flex-1 min-w-0 ms-4">
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                      Fecha:
+                    </p>
+                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                      {source.fechaPedido}
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li className="py-3 sm:py-4">
+                <div className="flex items-center ">
+                  <div className="flex-1 min-w-0 ms-4">
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                      Chofer asignado:
+                    </p>
+                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                      {source.chofer}
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex justify-end space-x-4 mt-4">
+            <button
+              className="text-black mt-4 focus:ring-4 focus:outline-none bg-white gap-x-2 hover:bg-grey-500 focus:ring-grey font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center hover:bg-gray-100 border border-gray-300"
+              onClick={onClose}
+            >
+              Cerrar
+            </button>
+
+            <button
+              className="text-white mt-4 focus:ring-4 focus:outline-none bg-yellow-400 gap-x-2 hover:bg-yellow-500 focus:ring-yellow-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+              onClick={handleSave}
+            >
+              Confirmar pedido
+            </button>
+          </div>
+        </div>
+      </div>
+      <PedidoGuardadoModal
+        show={showPedidoGuardadoModal}
+        message={"Pedido guardado"}
+      />
+    </>
+  );
+}
+
+export default ConfirmacionModal;
